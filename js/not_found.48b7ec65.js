00823838db3da21c274c7e2e66c5832904655d9b
hunterll2 <hunterll2@users.noreply.github.com>
2021-08-07 08:03:37 +0300

    deploy

---
+++ js/not_found.48b7ec65.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["not_found"],{9703:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("v-card",[a("v-card-title",[t._v(" Oppps! ")]),a("v-card-text",[t._v(" The page that you are looking for are not exists. ")])],1)},r=[],o=a("2877"),c=a("6544"),l=a.n(c),d=a("b0af"),s=a("99d9"),u={},i=Object(o["a"])(u,n,r,!1,null,null,null);e["default"]=i.exports;l()(i,{VCard:d["a"],VCardText:s["b"],VCardTitle:s["c"]})}}]);
+//# sourceMappingURL=not_found.48b7ec65.js.map
